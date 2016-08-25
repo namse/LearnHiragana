c144ee1c758e2b89711b5ae3e039cf256adb3349
skatpgusskat <skatpgusskat@naver.com>
2016-08-25 21:25:00 +0900

    Set up default react settings

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './App/main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react'],
+      },
+    }],
+  },
+};
